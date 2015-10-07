b7e22a339792842dabd814ea7a5f67c6962c9588
nikhilpi <nikhilpai2016@u.northwestern.edu>
2015-10-07 15:45:14 -0500

    testing on prod

--- server/publications/UsersPub.js
+++ server/publications/UsersPub.js
@@ -11,12 +11,14 @@ Meteor.methods({
     'getFBPhoto': function(){
       var user = Meteor.user();
       FBGraph.setAccessToken(user.services.facebook.accessToken);
-      FBGraph.get("me/picture?type=large", function(res){
-        Meteor.users.update(user._id, {
-          $set: {
-            'profile.picture': res.location
-          }
-        });
+      FBGraph.get("me/picture?type=large", function(res, err){
+        console.log(res);
+        console.log(err);
+        // Meteor.users.update(user._id, {
+        //   $set: {
+        //     'profile.picture': res.location
+        //   }
+        // });
       });
     }
 });
