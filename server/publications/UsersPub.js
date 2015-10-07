067e6e57d0bac49dc9b6fddd86c8f3ddf7c0eaab
nikhilpi <nikhilpai2016@u.northwestern.edu>
2015-10-07 15:48:02 -0500

    server side image grabber method

--- server/publications/UsersPub.js
+++ server/publications/UsersPub.js
@@ -11,14 +11,12 @@ Meteor.methods({
     'getFBPhoto': function(){
       var user = Meteor.user();
       FBGraph.setAccessToken(user.services.facebook.accessToken);
-      FBGraph.get("me/picture?type=large", function(res, err){
-        console.log(res);
-        console.log(err);
-        // Meteor.users.update(user._id, {
-        //   $set: {
-        //     'profile.picture': res.location
-        //   }
-        // });
+      FBGraph.get("me/picture?type=large", function(err, res){
+        Meteor.users.update(user._id, {
+          $set: {
+            'profile.picture': res.location
+          }
+        });
       });
     }
 });
